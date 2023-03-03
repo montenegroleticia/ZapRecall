3cabd72f69e01d82f382b9d0e0e7dda0c3d11a2a
Letícia Montenegro <montenegro.letiicia@gmail.com>
2023-03-03 20:45:50 -0300

    LandingPage done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import Home from "./Components/Home";
+import LandingPage from "./Components/LandingPage";
 
 export default function App() {
+  const [click, setClick] = useState();
 
   return (
     <>
-    <Home />
+    {click ? <Home /> : <LandingPage setClick={setClick} />}
     </>
   );
 }
---
+++ src/Components/LandingPage.js
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import logo from "../assets/logo.png";
+
+export default function LandingPage({ setClick }) {
+  return (
+    <Land>
+      <img src={logo} alt="logo" />
+      <h1>ZapRecall</h1>
+      <button onClick={() => setClick("ok")}>Iniciar Recall</button>
+    </Land>
+  );
+}
+const Land = styled.div`
+  width: auto;
+  height: 667px;
+  background: #fb6b6b;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  img {
+    margin-top: 148px;
+    width: 136px;
+    height: 161px;
+  }
+  h1 {
+    margin-top: 13px;
+    font-family: "Righteous";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 36px;
+    line-height: 45px;
+    letter-spacing: -0.012em;
+    color: #ffffff;
+  }
+  button {
+    margin-top: 30px;
+    width: 246px;
+    height: 54px;
+    color: #d70900;
+    background: #ffffff;
+    border: 1px solid #d70900;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
+    border-radius: 5px;
+  }
+`;
